b97985c1bc6fbd89ad74606a6b8cb2242784fffc
Fola Ajayi <foljai@yahoo.com>
2023-03-22 04:13:14 -0700

    my commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,19 @@
 #include "main.h"
 
+/**
+ * print_alphabet - print alphabet of letters
+ *
+ * Return: void
+ */
 
-	/**
-	 * print_alphabet - print all alphabet in lowercase
-	 */
+void print_alphabet(void)
+{
+	int i;
 
+	for (i = 'a'; i <= 'z'; i++)
 
-	void print_alphabet(void)
 	{
-		char letter;
-
-
-		for (letter = 'a'; letter <= 'z'; letter++)
-			_putchar(letter);
-
-
-		_putchar('\n');
+		_putchar(i);
 	}
-
+	_putchar('\n');
+}
